7f7d32485d75a229e5d44226ca9dff03f8194bad
b1nj <b.simon@pixellweb.com>
2014-12-03 10:46:45 -0400

    markdown boutons

--- public/js/editeur/markitup/sets/markdown/set.js
+++ public/js/editeur/markitup/sets/markdown/set.js
@@ -15,20 +15,20 @@ mySettings = {
     resizeHandle: false,
     onShiftEnter:       {keepDefault:false, openWith:'\n\n'},
     markupSet: [
-        {name:'Titre de niveau 2', key:'2', openWith:'## ', placeHolder:'Votre titre ici...' },
-        {name:'Titre de niveau 3', key:'3', openWith:'### ', placeHolder:'Votre titre ici...' },
-        {name:'Titre de niveau 4', key:'4', openWith:'#### ', placeHolder:'Votre titre ici...' },
+        {name:'Titre de niveau 2', key:'2', openWith:'## ', placeHolder:'Votre titre ici...', className:'markItUpButton-h2' },
+        {name:'Titre de niveau 3', key:'3', openWith:'### ', placeHolder:'Votre titre ici...', className:'markItUpButton-h3' },
+        {name:'Titre de niveau 4', key:'4', openWith:'#### ', placeHolder:'Votre titre ici...', className:'markItUpButton-h4' },
         {separator:'---------------' },
-        {name:'Gras', key:'B', openWith:'**', closeWith:'**'},
-        {name:'Italique', key:'I', openWith:'_', closeWith:'_'},
+        {name:'Gras', key:'B', openWith:'**', closeWith:'**', className:'markItUpButton-b'},
+        {name:'Italique', key:'I', openWith:'_', closeWith:'_', className:'markItUpButton-em'},
         {separator:'---------------' },
-        {name:'Liste à puce', openWith:'- ' },
+        {name:'Liste à puce', openWith:'- ', className:'markItUpButton-li' },
         {name:'Liste à chiffre', openWith:function(markItUp) {
             return markItUp.line+'. ';
-        }},
+        }, className:'markItUpButton-ol'},
         {separator:'---------------' },
-        {name:'Image', key:'P', replaceWith:'![[![Texte alternatif]!]]([![Url:!:http://]!])'},
-        {name:'Lien', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!])', placeHolder:'Texte du lien...' },
+        {name:'Image', key:'P', replaceWith:'![[![Texte alternatif]!]]([![Url:!:http://]!])', className:'markItUpButton-img'},
+        {name:'Lien', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!])', placeHolder:'Texte du lien...', className:'markItUpButton-a' },
         {separator:'---------------'},
         {
             name:'Aide',
@@ -36,7 +36,7 @@ mySettings = {
                 // markitub a été modifié ligne 223 pour avoir le contexte
                 $(e).parents('.markItUp').parent().parent().find('.markItUpHelp').toggle();
             },
-            className:'help'
+            className:'markItUpButton-help'
         }
     ]
 }
