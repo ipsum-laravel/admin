a603b543bc3f2ca05a5b87602d1899d809ec7ca3
B1nj <postmaster@b1nj.fr>
2015-11-09 15:18:41 -0400

    fix fautes

--- src/views/emails/auth/reminder.php
+++ src/views/emails/auth/reminder.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <h2>Récupèration de votre mot de passe</h2>
+        <h2>Récupération de votre mot de passe</h2>
 
         <p>
-            Pour récupèrer votre mot de passe veuillez utiliser ce lien :<br>
+            Pour récupérer votre mot de passe veuillez utiliser ce lien :<br>
             <?= link_to_route('admin.reset', null, array($token)) ?>
         </p>
     </body>
